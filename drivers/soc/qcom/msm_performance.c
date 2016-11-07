653b3967e765f9de806dc9237aa2069ba54702c7
engstk <eng.stk@sapo.pt>
2016-11-07 14:17:05 +0000

    msm_performance: don't let userspace change min/max freq policy by default
    simplify touchboost check and process
    
    Signed-off-by: engstk <eng.stk@sapo.pt>
    Signed-off-by: UtsavisGreat <utsavbalar1231@gmail.com>

--- drivers/soc/qcom/msm_performance.c
+++ drivers/soc/qcom/msm_performance.c
@@ -29,6 +29,7 @@
 static unsigned int use_input_evts_with_hi_slvt_detect;
 static struct mutex managed_cpus_lock;
 
+static int touchboost = 0;
 
 /* Maximum number to clusters that this module will manage*/
 static unsigned int num_clusters;
@@ -403,10 +404,9 @@ static int set_cpu_min_freq(const char *buf, const struct kernel_param *kp)
 	struct cpufreq_policy policy;
 	cpumask_var_t limit_mask;
 	int ret;
-	const char *reset = "0:0 1:0 2:0 3:0";
-
+	
 	if (touchboost == 0)
-		cp = reset;
+		return 0;
 
 	while ((cp = strpbrk(cp + 1, " :")))
 		ntokens++;
@@ -491,6 +491,9 @@ static int set_cpu_max_freq(const char *buf, const struct kernel_param *kp)
 	cpumask_var_t limit_mask;
 	int ret;
 
+	if (touchboost == 0)
+		return 0;
+	
 	while ((cp = strpbrk(cp + 1, " :")))
 		ntokens++;
 
